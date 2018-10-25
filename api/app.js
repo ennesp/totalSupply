821bb7e3528696d2ca6f74997c2dfbd46167e2a5
Enes Plojovic <enes.plojovic1@gmail.com>
2018-10-25 12:22:19 +0200

    decimals added

--- api/app.js
+++ api/app.js
@@ -7,12 +7,45 @@ const app = express();
 var web3 = new Web3(Web3.givenProvider || config.apiURL);
 
 var contractInstance = new web3.eth.Contract(config.tokenAbi, config.contractAddress);
+var bankInstance = new web3.eth.Contract(config.bankAbi, config.bankAddress);
+
+var tokenDecimals = 4;
+var decimals = (10 ** tokenDecimals);
 
 app.get('/', function(req, res){
+    bankInstance.methods.distributedTokens().call().then(function(distributedTokens) {
+        contractInstance.methods.totalSupply().call().then(function(totalSupply) {
+          //calculate
+          var bankedSupply = totalSupply / 2; //how much tokens locked in bank, by design 50%
+          var circulatingSupply = parseInt(totalSupply) - parseInt(bankedSupply) + parseInt(distributedTokens);
+          var realSupply = circulatingSupply/decimals;
+          var value = {
+              totalSupply: totalSupply,
+              circulatingSupply: realSupply
+          }
+
+          res.send(value)
+        });
+      });
+});
+
+app.get('/totalSupply', function(req, res){
     contractInstance.methods.totalSupply().call().then(function(value) {
         const total = {totalSupply: value};
         res.send(total)
-    }); 
+    });
+});
+
+app.get('/circulatingSupply', function(req, res){
+    bankInstance.methods.distributedTokens().call().then(function(distributedTokens) {
+        contractInstance.methods.totalSupply().call().then(function(totalSupply) {
+          //calculate
+          var bankedSupply = totalSupply / 2; //how much tokens locked in bank, by design 50%
+          var circulatingSupply = parseInt(totalSupply) - parseInt(bankedSupply) + parseInt(distributedTokens);
+          var realSupply = circulatingSupply/decimals;
+          res.send(realSupply.toString());
+        });
+      });
 });
 
 const port = process.env.PORT || 5000;
